df3576210cf71e84c125b2ca4c1805e58464cc3b
Nikita <NikMaslukov@gmail.com>
2018-04-23 20:56:08 +0300

    wda

--- src/main/java/com/example/demo/PersonControlApplication.java
+++ src/main/java/com/example/demo/PersonControlApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class PersonControlApplication implements CommandLineRunner {
+public class PersonControlApplication {
 
 
 	public static void main(String[] args) {
@@ -15,13 +14,7 @@ public class PersonControlApplication implements CommandLineRunner {
 
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		//accesRepo.save(new Person(0, "admin", "admin", 1, "ROLE_ADMIN", "admin", "admin"));
-		//accesRepo.save(new Person(0, "user", "user", 2, "ROLE_USER", "user", "user"));
-		//accesRepo.save(new Person(0, "test", "DWAWDA", 22, "ROLE_USER", "DWAWDA", "DWAWDA"));
-		
-	}
+
 
 
 }
