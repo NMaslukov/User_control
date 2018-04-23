02ce7b1fe27c7c845446b57e697d898bcca32e66
Nikita <NikMaslukov@gmail.com>
2018-04-23 19:53:10 +0300

    Hello

--- src/main/java/com/example/demo/PersonControlApplication.java
+++ src/main/java/com/example/demo/PersonControlApplication.java
@@ -1,21 +1,15 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import com.example.demo.dao.DaoAccesRepo;
-import com.example.demo.entity.Person;
-
 @SpringBootApplication
 @EnableCaching
 public class PersonControlApplication implements CommandLineRunner {
 
-	@Autowired
-	DaoAccesRepo accesRepo;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(PersonControlApplication.class, args);
 
@@ -23,9 +17,9 @@ public class PersonControlApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		accesRepo.save(new Person(0, "admin", "admin", 1, "ROLE_ADMIN", "admin", "admin"));
-		accesRepo.save(new Person(0, "user", "user", 2, "ROLE_USER", "user", "user"));
-		accesRepo.save(new Person(0, "test", "DWAWDA", 22, "ROLE_USER", "DWAWDA", "DWAWDA"));
+		//accesRepo.save(new Person(0, "admin", "admin", 1, "ROLE_ADMIN", "admin", "admin"));
+		//accesRepo.save(new Person(0, "user", "user", 2, "ROLE_USER", "user", "user"));
+		//accesRepo.save(new Person(0, "test", "DWAWDA", 22, "ROLE_USER", "DWAWDA", "DWAWDA"));
 		
 	}
 
--- src/main/java/com/example/demo/auth/Roles.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.auth;
-
-public interface Roles {
-	public final String ROLE_ADMIN = "ROLE_ADMIN";
-	public final String ROLE_USER = "ROLE_USER";
-
-}
