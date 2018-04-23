ffda7866090e153c8794f5bbf1f85abd6948354f
Nikita <NikMaslukov@gmail.com>
2018-04-23 22:50:12 +0300

    DWAWDADW

--- src/main/java/com/example/demo/PersonControlApplication.java
+++ src/main/java/com/example/demo/PersonControlApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 import com.example.demo.dao.DaoAccesRepo;
-import com.example.demo.entity.Person;
 
 @SpringBootApplication
 @EnableCaching
@@ -23,9 +22,9 @@ public class PersonControlApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		accesRepo.save(new Person(0, "admin", "admin", 1, "ROLE_ADMIN", "admin", "admin"));
-		accesRepo.save(new Person(0, "user", "user", 2, "ROLE_USER", "user", "user"));
-		accesRepo.save(new Person(0, "test", "DWAWDA", 22, "ROLE_USER", "DWAWDA", "DWAWDA"));
+		//accesRepo.save(new Person(0, "admin", "admin", 1, "ROLE_ADMIN", "admin", "admin"));
+	//	accesRepo.save(new Person(0, "user", "user", 2, "ROLE_USER", "user", "user"));
+	//	accesRepo.save(new Person(0, "test", "DWAWDA", 22, "ROLE_USER", "DWAWDA", "DWAWDA"));
 		
 	}
 
