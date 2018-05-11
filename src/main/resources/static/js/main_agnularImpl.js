11522de2a6f5565199d3d00066acf0664e57ed69
Nikita <NikMaslukov@gmail.com>
2018-05-11 23:49:20 +0300

    dwdwwda

--- src/main/resources/static/js/main_agnularImpl.js
+++ src/main/resources/static/js/main_agnularImpl.js
@@ -4,7 +4,7 @@ app.controller("AppCtrl", function($scope, $http, $window) {
  
 
 
-    $http.get('http://api.stackexchange.com/2.2/sites?filter=!2--Yion.2OJSStcKSpFvq').success(function(response) {
+    $http.get('https://api.stackexchange.com/2.2/sites?filter=!2--Yion.2OJSStcKSpFvq').success(function(response) {
         $scope.sites = response.items;
 		
     });
